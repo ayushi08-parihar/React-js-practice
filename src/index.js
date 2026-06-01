c99abb697081d92e5b12d155aa890a87b48d16d8
ayushi08-parihar <parihar9ayushi@gmail.com>
2026-06-01 15:25:59 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+/*function Greeting(){
+    //return <h1>Hello World</h1>;
+    return <h2 id="greeting"
+    className="greeting" ></h2>;
+}*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//CREATEELEMNT
+/*function Greeting() {
+  return (
+    <div>
+      <h2>hello world</h2>
+    </div>
+  );
+}*/
+
+
+//CREATEELEMENT USE DIV
+/*const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "hello world"),
+  );
+};*/
+//FORMATTING ,CLASSNAME
+/*function Greeting() {
+  return (
+    <>
+      <div className="HII">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input type=' text' name='' id='' />
+    </>
+  );
+}*/
+
+
+//NEXT COMPONENT
+/*function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};*/
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+    alt="Interesting Facts For Curious Minds"
+  />
 );
+const Title = () => {
+  return <h2>Interesting Facts For Curious Minds</h2>;
+};
+const Author = () => <h4>Jordan Moore </h4>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
