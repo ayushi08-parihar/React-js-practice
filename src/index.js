006e745aecd476ec359ae8a26d07b954d409804f
ayushi08-parihar <parihar9ayushi@gmail.com>
2026-06-02 21:46:27 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -57,7 +57,9 @@ const Person = () => <h2>john doe</h2>;
 const Message = () => {
   return <p>this is my message</p>;
 };*/
-function BookList() {
+
+import "./index.css";
+/*function BookList() {
   return (
     <section>
       <Book />
@@ -68,12 +70,24 @@ function BookList() {
   );
 }
 
-const Book = () => {
+/*const Book = () => {
   return (
     <article className="book">
       <Image />
       <Title />
       <Author />
+    </article>
+  );
+};*/
+/*const Book = () => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+        alt="Interesting Facts For Curious Minds"
+      />
+      <h2>Interesting Facts For Curious Minds</h2>
+      <h4>Jordan Moore </h4>
     </article>
   );
 };
@@ -87,8 +101,180 @@ const Image = () => (
 const Title = () => {
   return <h2>Interesting Facts For Curious Minds</h2>;
 };
-const Author = () => <h4>Jordan Moore </h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+    Jordan Moore
+  </h4>
+);*/
 
+const author = "Jordan Moore";
+const title = "Interesting Facts For Curious Minds";
+const img = "./images/book-1.jpg";
+
+/*function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+    </section>
+  );
+}
+/*const Book = () => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+    </article>
+  );
+};*/
+// parameters
+/*const someFunc = (param1, param2) => {
+  console.log(param1, param2);
+};
+// arguments
+someFunc('job', 'developer');
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+      {console.log(props)}
+    </article>
+  );
+};*/
+/*function BookList() {
+  return (
+    <section className="booklist">
+      <Book job="developer" />
+      <Book title="random title" number={22} />
+    </section>
+  );
+}
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  );
+};*/
+/*
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book author={author} title={title} img={img} />
+      <Book title={title} img={img} />
+    </section>
+  );
+}
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
+    </article>
+  );
+};
+*/
+
+const firstBook = {
+  author: "Jordan Moore",
+  title: "Interesting Facts For Curious Minds",
+  img: "./images/book-1.jpg",
+};
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+};
+/*
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  );
+}
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
+  /*return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
+    </article>
+  );*/
+ /* return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+    </article>
+  );
+};*/
+
+
+
+
+//CHILDREN PROPS
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button>click me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  );
+}
+
+/*const Book = ({ img, title, author, children }) => {
+  // rest of the logic
+};*/
+const Book = (props) => {
+  const { img, title, author, children } = props;
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+      {children}
+    </article>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
